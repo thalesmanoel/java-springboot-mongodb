5fc8e32152f21fe35ee98f9677ecbe07fff84582
Thales Carrion <thalesdesenvolvedor@gmail.com>
2024-10-17 17:57:13 -0300

    Connecting MongoDB

---
+++ src/main/java/com/example/demo/resources/UserResource.java
@@ -0,0 +1,26 @@
+package com.example.demo.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.domain.User;
+import com.example.demo.services.UserService;
+
+@RestController
+@RequestMapping(value="/users")
+public class UserResource {
+
+	@Autowired
+	private UserService userService;
+	
+	@GetMapping
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = userService.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+}
